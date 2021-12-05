eb1b28a84e153c4cd5e67ddee3f722539c086e2c
Kheiner Ivan Murillo Mosquera <88347552+kheinermm@users.noreply.github.com>
2021-12-04 22:38:47 -0500

    router producto y venta

--- models/ventas.model.js
+++ models/ventas.model.js
@@ -7,7 +7,8 @@ const VentasSchema = new Schema({
     nom_producto: { type: String, required: true, max: 60 },
     cantidad: { type: String, required: true, max: 10 },
     precio_unit: { type: String, required: true, max: 15 },
-    precio_total: { type: String, required: true, max: 15 }
+    precio_total: { type: String, required: true, max: 15 },
+    descuento: { type: String, required: false, max: 15 }
 });
 
 module.exports = mongoose.model("ventas", VentasSchema);

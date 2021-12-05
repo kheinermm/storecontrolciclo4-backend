d808971e76b47a1d96862e641341355d2240e2e9
Kheiner Ivan Murillo Mosquera <88347552+kheinermm@users.noreply.github.com>
2021-12-05 13:00:31 -0500

    ok

--- config/database.js
+++ config/database.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose");
 const host = "localhost";
 const port = "27017";
 const db = "storecontrolc4";
+const hostdb = "cluster0.bhqtr.mongodb.net";
+const userdb = "misiontic2022";
+const passdb = "xdKk82aY7dMmqs2s";
 
 exports.mongoConnect = () => {
-    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+
+    // const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+    // const mongoStringConnection = `mongodb+srv://${userdb}:${passdb}@${hostdb}/${db}`;
+    const mongoStringConnection = `mongodb+srv://${userdb}:${passdb}@${hostdb}/${db}?retryWrites=true&w=majority`;
 
     mongoose.connect(mongoStringConnection);
     mongoose.Promise = global.Promise;
